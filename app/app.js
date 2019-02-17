8eb0b2a06292e50c42f28e120e1a8605523a0bfe
wtitze <w.titze@gmail.com>
2019-02-17 11:38:08 +0000

    primo commit

--- app/app.js
+++ app/app.js
@@ -1,12 +1,28 @@
-/*eslint-env node*/
+// Realizzazione di una chat con socket.io
 
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res){ // alla richiesta della homepage
+  res.sendFile(__dirname + '/index.html'); // restituisce il file index.html che contiene quanto necessario per la chat
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function(socket){ // gestisce le connessioni
+  
+  console.log('a user connected');  
+  
+  socket.on('chat message', function(msg){ // quando viene ricevuto l'evento 'chat message'
+    console.log('message: ' + msg); // viene visualizzato il messaggio ricevuto sulal console
+    io.emit('chat message', msg); // viene rispedito a tutti i client connessi
+  });
+  
+  socket.on('disconnect', function(){ // quando viene ricevuto l'evento 'disconnect'
+    console.log('user disconnected'); // viene visualizzato il messaggio che un utente si è disconnesso sulla console del server
+  });
+  
+});
+
+http.listen(3000, function(){ // Attenzione: non app.listen() ma http.listen() perché socket.io è utilizza il server http
+  console.log('listening on port 3000');
 });
